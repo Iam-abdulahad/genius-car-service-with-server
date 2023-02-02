f8004df95bba2bf7a2c3be8243e2085001ca5ed9
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-02-02 07:48:19 +0600

    showing experts on home page

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -13,9 +13,9 @@ const Services = () => {
       .then(data => setServices(data));
   }, [])
   return (
-    <div>
+    <div className='container'>
       <div>
-      <h2 className='text-primary text-center mt-5'>Our Services</h2>
+        <h2 className='text-primary text-center mt-5'>Our Services</h2>
         <div className='services-container'>
           {
             services.map(service => <Service
