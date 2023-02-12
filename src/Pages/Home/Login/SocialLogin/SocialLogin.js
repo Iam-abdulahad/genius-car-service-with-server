f89ef31fb67598eece401528058e80b012354634
Abdul Ahad <ahmmedsumon087@gmai.com>
2023-02-12 15:42:31 +0600

    emplement githun and facebook login system

--- src/Pages/Home/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Home/Login/SocialLogin/SocialLogin.js
@@ -2,27 +2,28 @@ import React from 'react';
 import googleLogo from '../../../../images/social-icon/google.png';
 import facebookLogo from '../../../../images/social-icon/facebook.png';
 import githubLogo from '../../../../images/social-icon/github.png';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../../firebase.init';
 import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
           const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+          const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+          const [signInWithFacebook, user2, loading2, error2] = useSignInWithFacebook(auth);
 
 
 
-
-          if (error) {
+          if (error || error1 || error2) {
                     return (
                               <div>
                                         <p>Error: {error.message}</p>
                               </div>
                     );
           }
-          if (loading) {
+          if (loading || loading1 || loading2) {
                     return <p>Loading...</p>;
           }
-          if (user) {
+          if (user || user1 || user2) {
                     return (
                               <div>
                                         <p>Signed In User: {user.email}</p>
@@ -46,11 +47,13 @@ const SocialLogin = () => {
                                                   <span className='px-2'>Google Sign In</span>
                                         </button>
                                         <button
+                                                  onClick={() => signInWithFacebook() }
                                                   className='btn btn-info d-block w-100 mx-auto my-2'>
                                                   <img style={{ width: '30px' }} src={facebookLogo} alt="" />
                                                   <span className='px-2'>Facebook Sign In</span>
                                         </button>
                                         <button
+                                                  onClick={() => signInWithGithub() }
                                                   className='btn btn-info w-100 d-block mx-auto'>
                                                   <img style={{ width: '30px' }} src={githubLogo} alt="" />
                                                   <span className='px-2'>Github Sign In</span>
